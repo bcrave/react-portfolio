fbca07e9c3cfdcd51b8c8e34fc6e0210f76d266f
bcrave <brennycode@gmail.com>
2020-01-08 13:19:44 -0700

    fixed some directory errors

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,72 +1,81 @@
 import React, { Component } from "react";
-import axios from "axios"
-
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
-   
-        this.state = {
-            pageTitle: "Welcome to my portfolio",
-            isLoading: false,
-            data: []
-          };
+  constructor() {
+    super();
 
-          this.handleFilter = this.handleFilter.bind(this)
-          
-        }
-      
-        portfolioItems() {
-          return this.state.data.map(item => {
-            return <PortfolioItem key={item.id} title={item.name} url={item.url} slug={item.id} />;
-          });
-        }
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
 
-        handleFilter(filter) {
-          this.setState({
-            data: this.state.data.filter(item => {
-              return item.category === filter 
-            })
-          })
-        }
+    this.handleFilter = this.handleFilter.bind(this);
+  }
 
-        getPortfolioItems() {
-          axios
-          .get('https://brendoncrave.devcamp.space/portfolio/portfolio_items')
-          .then(response => {
-          // handle success
-            console.log("response data", response);
-            this.setState({
-              data: response.data.portfolio_items
-            })
-          })
-          .catch(error => {
-          // handle error
-            console.log(error);
-          });
-        }
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
 
-        componentDidMount() {
-          this.getPortfolioItems()
-        }
+  getPortfolioItems() {
+    axios
+      .get("https://brendoncrave.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        this.setState({
+          data: response.data.portfolio_items
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-    render() {
-      
-      if (this.state.isLoading) {
-        return <div>Loading...</div>
-      }
-        return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
+  portfolioItems() {
+    return this.state.data.map(item => {
+      console.log("portfolio item", item);
+      return (
+        <PortfolioItem
+          key={item.id}
+          title={item.name}
+          url={item.url}
+          slug={item.id}
+        />
+      );
+    });
+  }
 
-                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
 
-                {this.portfolioItems()}
-            </div>
-        );
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
+
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
+
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
+
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
