4401508162cafbd6557c53e8bd7e525c877f8daa
bcrave <brennycode@gmail.com>
2020-01-08 10:41:25 -0700

    added axios

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -10,17 +10,17 @@ export default class PortfolioContainer extends Component {
             pageTitle: "Welcome to my portfolio",
             isLoading: false,
             data: [
-              { title: "Quip", category: "eCommerce" },
-              { title: "Eventbrite", category: "Scheduling" },
-              { title: "Ministry Safe", category: "Enterprise" },
-              { title: "SwingAway", category: "eCommerce" }
+              { title: "Quip", category: "eCommerce", slug: "quip" },
+              { title: "Eventbrite", category: "Scheduling", slug: "eventbrite" },
+              { title: "Ministry Safe", category: "Enterprise", slug: "ministry-safe" },
+              { title: "SwingAway", category: "eCommerce", slug: "swingaway" }
             ]
           };
         }
       
         portfolioItems() {
           return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"} />;
+            return <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />;
           });
         }
 
@@ -45,8 +45,6 @@ export default class PortfolioContainer extends Component {
                 <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
 
                 {this.portfolioItems()}
-
-                <button onClick={this.handlePageTitleUpdate}>Change Title</button>
             </div>
         );
     }
